64ca17e7977313c89b3d2d372e49757ac5afd043
MohsenAlMahmud <mohsen.al.mahmud@gmail.com>
2023-10-08 01:39:42 +0600

    why choose us and customers love routes added completely

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -8,8 +8,8 @@ const Banner = () => {
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <h1 className="mb-5 text-7xl font-bold text-white">RC Creative</h1>
+                        <p className="text-4xl mb-5 text-white">WE KEEP YOUR MEMORIES ALIVE....</p>
                         <Link to={'/services'}><button className="btn btn-primary">Our Services</button></Link>
                     </div>
                 </div>
--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,23 +2,26 @@
 
 const Feedback = () => {
     return (
-        <div>
-            <div>
-                <h4>Sarah's Special Day:</h4>
-                <p>"Our wedding day was pure magic, thanks to [Your Company]. - Sarah"</p>
-                <h4>John & Emily's Bliss:</h4>
-                <p>"A decade of love celebrated flawlessly by [Your Team]. - John & Emily"</p>
-                <h4>Sophia's Birthday Delight:</h4>
-                <p>"Our daughter's party was an absolute hit, all thanks to you! - Sophia"</p>
+        <div className="p-10">
+            <h2 className="text-5xl text-center font-semibold py-5">Customers Love...</h2>
+            <div className="flex gap-10">
+            <div className="w-1/2">
+                <h4 className="text-xl font-semibold">Sarah's Special Day:</h4>
+                <p className="italic">"Our wedding day was pure magic, thanks to <span className="text-blue-500 italic">RC creative</span>. - Sarah"</p>
+                <h4 className="text-xl font-semibold">John & Emily's Bliss:</h4>
+                <p className="italic">"A decade of love celebrated flawlessly by <span className="text-blue-500 italic">RC creative</span>. - John & Emily"</p>
+                <h4 className="text-xl font-semibold">Sophia's Birthday Delight:</h4>
+                <p className="italic">"Our daughter's party was an absolute hit, all thanks to you! - Sophia"</p>
             </div>
-            <div>
-                <h4>Oliver's Party Perfection:</h4>
-                <p>"You turned our vision into a party beyond our dreams! - Oliver"</p>
-                <h4>Grace's Seamless Celebrations:</h4>
-                <p>"Every detail was flawless, and our event was unforgettable. - Grace"</p>
-                <h4>Michael's Memories Forever:</h4>
-                <p>"Thanks to your team, we now have cherished memories. - Michael"</p>
+            <div className="w-1/2">
+                <h4 className="text-xl font-semibold">Oliver's Party Perfection:</h4>
+                <p className="italic">"You turned our vision into a party beyond our dreams! - Oliver"</p>
+                <h4 className="text-xl font-semibold">Grace's Seamless Celebrations:</h4>
+                <p className="italic">"Every detail was flawless, and our event was unforgettable. - Grace"</p>
+                <h4 className="text-xl font-semibold">Michael's Memories Forever:</h4>
+                <p className="italic">"Thanks to your team, we now have cherished memories. - Michael"</p>
             </div>
+        </div>
         </div>
     );
 };
